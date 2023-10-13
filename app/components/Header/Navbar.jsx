1f071522a5165f628b4ef6a5ab57bc9bf6a96ce0
Maria <sawaby@users.noreply.github.com>
2023-10-13 09:52:54 -0700

    rearranging file import

--- app/components/Header/Navbar.jsx
+++ app/components/Header/Navbar.jsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import Image from 'next/image';
 import NavLink from './NavLink';
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from './MenuOverlay';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 
 const navLinks = [
     {
